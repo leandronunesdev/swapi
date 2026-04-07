72f4872eacff804595aa15c25c6654ccb6ac971b
Leandro <nunes.pessoal@gmail.com>
2026-04-07 18:56:05 -0300

    fix: adjust apollo client registration uri

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -2,10 +2,15 @@ import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
 import { registerApolloClient } from "@apollo/experimental-nextjs-app-support/rsc";
 
 export const { getClient } = registerApolloClient(() => {
+  const configuredUri = process.env.SWAPI_URL ?? "";
+  const normalizedUri = configuredUri.endsWith("/.netlify/functions/index")
+    ? configuredUri.replace("/.netlify/functions/index", "/graphql")
+    : configuredUri;
+
   return new ApolloClient({
     cache: new InMemoryCache(),
     link: new HttpLink({
-      uri: process.env.SWAPI_URL,
+      uri: normalizedUri,
     }),
   });
 });
